4f10b4bc5523f5d3b77718e4e79aba03950177d2
Lvnatic <thomaslinnemann55@gmail.com>
2025-06-18 10:36:38 +0200

    Initial commit

---
+++ src/main/java/net/lvnatic/atmxmsd/AllTheMonsXMegaShowdown.java
@@ -0,0 +1,16 @@
+package net.lvnatic.atmxmsd;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AllTheMonsXMegaShowdown implements ModInitializer {
+	public static final String MOD_ID = "atmxmsd";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lvnatic/atmxmsd/AllTheMonsXMegaShowdownClient.java
@@ -0,0 +1,10 @@
+package net.lvnatic.atmxmsd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AllTheMonsXMegaShowdownClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
